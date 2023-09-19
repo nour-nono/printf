178722a222c9ccf5b5fa4c2560492fe047cc245a
ahmedemc2 <ahmedtkitoemc2@gmail.com>
2023-09-19 20:48:00 +0000

    aaa

--- 0-printf.c
+++ 0-printf.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
 				handleBuffer(-1, "");
 				return (-1);
 			}
-			if (format[i] == ' ' && (format[i + 1] == '\0' || format[i + 1] == ' '))
+			if (format[i] == ' ' && format[i + 1] == '\0')
 			{
 				handleBuffer(-1, "");
 				return (-1);
