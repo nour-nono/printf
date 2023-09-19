c1a367cba852847cd07c0290a06ad23268cfd230
nono <nour98nour@gmail.com>
2023-09-20 00:17:42 +0300

    maybe yes maybe no

--- 0-functions.c
+++ 0-functions.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ *  _strlen - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		++i;
+	return (i);
+}
+/**
  * printChar - the function that print the unsigned value
  *
  * @argu : the number to be printed
@@ -26,7 +43,7 @@ void printString(va_list argu)
 
 	if (!st)
 		st = "(null)";
-	len = strlen(st);
+	len = _strlen(st);
 	handleBuffer(len, st);
 }
 /**
