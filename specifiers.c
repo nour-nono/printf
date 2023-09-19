abc320352f8a040f5a72a17fd9cc338d9ce2f3f9
ahmedemc2 <ahmedtkitoemc2@gmail.com>
2023-09-19 17:47:10 +0000

    specifiers format

---
+++ specifiers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * searchInSpecfires - the function that print the unsigned value
+ *
+ * @format : the number to be printed
+ * @flag : the number to be printed
+ * @ptr : the number to be printed
+ *
+ * Return: Nothing
+*/
+void searchInSpecfires(char format, int *flag, va_list ptr)
+{
+	placeHolders specifiers[] = {
+		{'c', printChar},
+		{'s', printString},
+		{'d', printInteger},
+		{'i', printInteger},
+		{'b', printBinary},
+		{'u', printUnsigned},
+		{'o', printOctal},
+		{'x', printHex},
+		{'X', printHEX},
+		{'S', printVisibleString},
+	};
+	int j = 0;
+
+	for (; j < 10; ++j)
+	{
+		if (format == specifiers[j].sp && *flag)
+			specifiers[j].funcPtr(ptr), *flag = 0;
+	}
+}
