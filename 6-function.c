9a84ca6e74baa003353dbeb1ba0ffdc82dc8a83f
ahmedemc2 <ahmedtkitoemc2@gmail.com>
2023-09-20 05:15:36 +0300

    a

--- 6-function.c
+++ 6-function.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * convertUnsignedLongToHex - the function that convert a number to hexadecimal
+ *
+ * @num : the number to be converted
+ * @flagChar : it determines if 'X' capital or small 'x'
+ *
+ * Return: Nothing
+ */
 void convertUnsignedLongToHex(unsigned long num, int flagChar)
 {
 	int x, y = (flagChar != 48) ? 16 : 8;
@@ -11,12 +18,19 @@ void convertUnsignedLongToHex(unsigned long num, int flagChar)
 	c = (x < 10) ? (x + 48) : (x + flagChar);
 	handleBuffer(1, &c);
 }
-
+/**
+ * printAdress - the function that convert a number to hexadecimal
+ *
+ * @argu : the number to be printed
+ *
+ * Return: Nothing
+ */
 void printAdress(va_list argu)
 {
 	void *p = va_arg(argu, void *);
 	char *st1 = "(nil)", *st2 = "0x";
 	unsigned long x;
+
 	if (!p)
 	{
 		handleBuffer(5, st1);
